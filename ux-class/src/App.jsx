a8d8a2af43469d756cb8cd181d1c3f79db1b7888
KS <180429671+ks9927@users.noreply.github.com>
2025-09-30 16:11:31 -0400

    added app content

--- ux-class/src/App.jsx
+++ ux-class/src/App.jsx
@@ -1,1 +1,35 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import './App.css'
+import Home from './Pages/Home'
+import Travel from './Pages/Travel'
+import Experiences from './Pages/Experiences'
+import Contact from './Pages/Contact'
+import FAQ from './Pages/FAQ'
+import Booking from './Pages/Booking'
+import Navbar from './Components/Navbar';
+import Layout from './Components/Layout';
+import Footer from './Components/Footer';
+import Sources from './Pages/Sources';
+
+function App() {
+  return (
+    <Router>
+        <Navbar />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="travel" element={<Travel />} />
+          <Route path="experiences" element={<Experiences />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="faq" element={<FAQ />} />
+          <Route path="booking" element={<Booking />} />
+           <Route path="sources" element={<Sources />} />
+        </Route>
+      </Routes>
+      <Footer/>
+    </Router>
+  )
+}
+
+export default App
 
